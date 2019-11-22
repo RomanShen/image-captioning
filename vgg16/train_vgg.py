a05c0b69c5104dc911608f50f72d23ace1459840
shen <siangching.shen@gmail.com>
2019-11-22 17:31:01 +0800

    finished training loop for train_vgg

--- vgg16/train_vgg.py
+++ vgg16/train_vgg.py
@@ -7,6 +7,8 @@ from ignite.engine import Events, create_supervised_evaluator, create_supervised
 from ignite.metrics import Accuracy, Loss
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import ExponentialLR
+from ignite.handlers import ModelCheckpoint, EarlyStopping, TerminateOnNan
 
 
 def create_summary_writer(model, data_loader, log_dir):
@@ -20,6 +22,11 @@ def create_summary_writer(model, data_loader, log_dir):
     return writer
 
 
+def default_score_fn(engine):
+    score = engine.state.metrics['Accuracy']
+    return score
+
+
 def run(train_loader, val_loader, epochs, lr, momentum, log_interval, log_dir):
 
     model = Vgg16()
@@ -30,20 +37,48 @@ def run(train_loader, val_loader, epochs, lr, momentum, log_interval, log_dir):
     if torch.cuda.is_available():
         device = 'cuda'
 
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=0.001)
+
+    lr_scheduler = ExponentialLR(optimizer, gamma=0.975)
 
     trainer = create_supervised_trainer(model, optimizer, F.nll_loss, device=device)
 
     evaluator = create_supervised_evaluator(model,
                                             metrics={'accuracy': Accuracy(),
                                                      'nll': Loss(F.nll_loss)},
-                                            device=device)\
+                                            device=device)
 
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda engine: lr_scheduler.step())
+
+    trainer.add_event_handler(Events.ITERATION_COMPLETED, TerminateOnNan())
+
+    # store the best model
+    best_model_handler = ModelCheckpoint(
+        dirname=log_dir,
+        filename_prefix="best",
+        n_saved=3,
+        score_name="test_acc",
+        score_function=default_score_fn
+    )
+    evaluator.add_event_handler(Events.COMPLETED, best_model_handler, {'model': model, })
+
+    # add early stopping
+    es_patience = 5
+    es_handler = EarlyStopping(patience=es_patience, score_function=default_score_fn, trainer=trainer)
+    evaluator.add_event_handler(Events.COMPLETED, es_handler)
+
+    def empty_cuda_cache(engine):
+        torch.cuda.empty_cache()
+        import gc
+        gc.collect()
+
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, empty_cuda_cache)
+    evaluator.add_event_handler(Events.COMPLETED, empty_cuda_cache)
 
     @trainer.on(Events.ITERATION_COMPLETED(every=log_interval))
     def log_training_loss(engine):
         print("Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
-              "".format(engine.state.epoch, iter, len(train_loader), engine.state.output))
+              "".format(engine.state.epoch, engine.state.iteration, len(train_loader), engine.state.output))
         writer.add_scalar("training/loss", engine.state.output, engine.state.iteration)
 
     @trainer.on(Events.EPOCH_COMPLETED)
@@ -81,9 +116,9 @@ if __name__ == '__main__':
     parser.add_argument('--num_epochs', type=int, default=30)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--momentum', type=float, default=0.5,
+    parser.add_argument('--momentum', type=float, default=0.8,
                         help='SGD momentum (default: 0.5)')
-    parser.add_argument('--log_interval', type=int, default=500,
+    parser.add_argument('--log_interval', type=int, default=200,
                         help='how many batches to wait before logging training status')
     parser.add_argument("--log_dir", type=str, default="tensorboard_logs",
                         help="log directory for Tensorboard log output")
