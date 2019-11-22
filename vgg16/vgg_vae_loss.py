3460ec292b2fe240b7c7416b930d3eed4b642a8c
shen <siangching.shen@gmail.com>
2019-11-22 12:20:40 +0800

    added vae loss function

---
+++ vgg16/vgg_vae_loss.py
@@ -0,0 +1,19 @@
+from torch import nn
+import torch
+
+
+class VAELoss(nn.Module):
+    def __init__(self, k1=0.1, k2=0.1):
+        super(VAELoss, self).__init__()
+        self.mse_loss = nn.MSELoss()
+        self.ce = nn.CrossEntropyLoss()
+        self.k1 = k1
+        self.k2 = k2
+
+    def forward(self, y_pred, y, recon_x, x, mu, logvar):
+        MSE = self.mse_loss(recon_x, x)
+        # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        KLD = torch.mean(torch.mul(mu, mu)) + torch.mean(torch.exp(logvar)) - torch.mean(logvar) - 1
+        CLS = self.ce(y_pred, y)
+
+        return CLS + self.k1 * MSE + self.k2 * KLD
