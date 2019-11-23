225b8cb044cdbcc13c3aea8c816b92d4b8cad414
shen <siangching.shen@gmail.com>
2019-11-23 20:00:00 +0800

    fixed vgg_vae loss function bug

--- vgg16/vgg_vae_loss.py
+++ vgg16/vgg_vae_loss.py
@@ -6,7 +6,7 @@ class VAELoss(nn.Module):
     def __init__(self, k1=0.1, k2=0.1):
         super(VAELoss, self).__init__()
         self.mse_loss = nn.MSELoss()
-        self.nll = F.nll_loss()
+        # self.nll = F.nll_loss()
         self.k1 = k1
         self.k2 = k2
 
@@ -14,6 +14,6 @@ class VAELoss(nn.Module):
         MSE = self.mse_loss(recon_x, x)
         # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         KLD = torch.mean(torch.mul(mu, mu)) + torch.mean(torch.exp(logvar)) - torch.mean(logvar) - 1
-        CLS = self.nll(y_pred, y)
+        # CLS = self.nll(y_pred, y)
 
-        return CLS + self.k1 * MSE + self.k2 * KLD
+        return F.nll_loss(y_pred, y) + self.k1 * MSE + self.k2 * KLD
